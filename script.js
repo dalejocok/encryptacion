3826a028ce74929a3e235ea162f7989ca6750c91
dalejocok <100045746+dalejocok@users.noreply.github.com>
2024-07-09 19:33:29 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,74 @@
+function encrypt() {
+    let inputText = document.getElementById('input-text').value.trim().toLowerCase();
+    let outputText = '';
+
+    for (let i = 0; i < inputText.length; i++) {
+        switch (inputText[i]) {
+            case 'e':
+                outputText += 'enter';
+                break;
+            case 'i':
+                outputText += 'imes';
+                break;
+            case 'a':
+                outputText += 'ai';
+                break;
+            case 'o':
+                outputText += 'ober';
+                break;
+            case 'u':
+                outputText += 'ufat';
+                break;
+            default:
+                outputText += inputText[i];
+        }
+    }
+
+    document.getElementById('output-text').value = outputText;
+}
+
+function decrypt() {
+    let inputText = document.getElementById('input-text').value.trim().toLowerCase();
+    let outputText = '';
+    let i = 0;
+
+    while (i < inputText.length) {
+        let char = inputText.substr(i, 5);
+
+        switch (char) {
+            case 'enter':
+                outputText += 'e';
+                i += 5;
+                break;
+            case 'imes':
+                outputText += 'i';
+                i += 4;
+                break;
+            case 'ai':
+                outputText += 'a';
+                i += 2;
+                break;
+            case 'ober':
+                outputText += 'o';
+                i += 4;
+                break;
+            case 'ufat':
+                outputText += 'u';
+                i += 4;
+                break;
+            default:
+                outputText += inputText[i];
+                i++;
+        }
+    }
+
+    document.getElementById('output-text').value = outputText;
+}
+
+function copyToClipboard() {
+    let outputText = document.getElementById('output-text');
+    outputText.select();
+    outputText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Texto copiado al portapapeles: " + outputText.value);
+}
